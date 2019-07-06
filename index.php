6a36cd70fbaa2c7461060a4bc1d92b75f052e487
Fajar Fathurrahman <32007820+fajarfathurrahman@users.noreply.github.com>
2019-07-07 03:25:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-    <h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-    <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
-    <?php if($_ENV["HOST_NAME"]) {?><h3>My hostname is <?php echo $_ENV["HOST_NAME"]; ?></h3><?php } ?>
+###    <h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1> ###
+    <?php if($_ENV["HOSTNAME"]) {?><h1>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h1><?php } ?>
+    <?php if($_ENV["HOST_NAME"]) {?><h1>My hostname is <?php echo $_ENV["HOST_NAME"]; ?></h1><?php } ?>
  </html>
